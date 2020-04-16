136d51826fbbfae139272072e4c0218de20371d1
DmitrySinitsin <sera@bankir.ru>
2020-04-16 11:17:05 +0300

    JS_001_console

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log('Hello');//строки берем в кавычки
+console.log("world");
+console.log("3333");
+console.log(334);
+console.log('Hello' + ' ' + 'world');//конкатенация
+console.info('Hello');
+//alert('Hello');
+// alert('Hello');
+//alert('Hello');
+
+document.getElementById('out').innerHTML = 'Hello';
+document.getElementById('out').innerHTML = 2019;
+document.getElementById('out').innerHTML = '<b>2020</b>';
+document.querySelector('.header').innerHTML = 15;
+document.querySelector('#one').innerHTML = 7777;
+document.getElementById('one').innerHTML = 888;
+
+let b;
+let a = document.querySelector('#one');
+let c;
+c = document.querySelector('.header');
+a.innerHTML = 999;
+c.innerHTML = 6666;
