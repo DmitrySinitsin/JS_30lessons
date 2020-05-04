84f1499d3acecb0f9417d287fd1729c14ad6a9d5
DmitrySinitsin <sera@bankir.ru>
2020-05-04 16:32:40 +0300

    JS2_019_mouseEvents

--- js/script.js
+++ js/script.js
@@ -1,18 +1,73 @@
-let a = 'hello, hi, mahai';
-console.log(a.split(','));
+document.querySelector('.one').onclick = function (event) {
+    console.log(event);
+    console.log('click');
+}
 
-let b = [8, 9, 7];
-console.log(b);
-console.log(b.join());
-console.log(b.join(':'));
+document.querySelector('.two').onclick = function () {
+    console.log('click2');
+}
+
+document.querySelector('.two').ondblclick = () => {
+    console.log('double click detected!')
+}
+
+document.querySelector('.two').oncontextmenu = () => {
+    console.log('right button');
+    return false;
+}
+
+// let w = 75;
+// document.querySelector('.three').onmousemove = () => {
+//     document.querySelector('.three').style.width = w + 'px';
+//     w++;
+// }
+
+document.querySelector('.three').onmouseenter = () => {
+    document.querySelector('.three').style.background = 'yellow';
+    console.log(1);
+}
+
+document.querySelector('.three').onmouseleave = () => {
+    document.querySelector('.three').style.background = 'green';
+    console.log(2);
+}
+
+document.querySelector('.three').onmousedown = () => {
+    document.querySelector('.three').style.background = 'orange';
+    console.log(3);
+}
+
+document.querySelector('.three').onmouseup = () => {
+    document.querySelector('.three').style.background = 'blue';
+    console.log(3);
+}
+
+let p = 10;
+document.querySelector('button').onclick = function (e) {
+    p++;
+    document.querySelector('progress').value = p;
+}
 
 
-b.forEach(function (elem, index) {
-    console.log(index);
-    console.log(elem * 2);
-    console.log('---');
-});
-console.log(b);
+
+
+
+// 018LESSON
+// let a = 'hello, hi, mahai';
+// console.log(a.split(','));
+
+// let b = [8, 9, 7];
+// console.log(b);
+// console.log(b.join());
+// console.log(b.join(':'));
+
+
+// b.forEach(function (elem, index) {
+//     console.log(index);
+//     console.log(elem * 2);
+//     console.log('---');
+// });
+// console.log(b);
 
 
 // let a = [4, 5, 12, 200, 1, 0, -2];
